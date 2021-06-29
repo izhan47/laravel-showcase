665714dd3f67e77703b4c16ecc602512d1cf110a
mumar82 <umar@softsquare.io>
2021-06-29 13:25:56 +0500

    Blade file added

--- app/Http/Controllers/Api/V1/UsersController.php
+++ app/Http/Controllers/Api/V1/UsersController.php
@@ -581,7 +581,7 @@ class UsersController extends Controller
     {
         $address = env('MAIL_TO_ADDRESS', 'softsquare.4td1gv@zapiermail.com');
 
-        Mail::send('emails.addSubscriber', ["detail" => $request], function ($m) use ($address, $request) {
+        Mail::send('emails.api.v1.addSubscriber', ["detail" => $request], function ($m) use ($address, $request) {
             $m->from($request->address, $request->name);
             $m->to($address);
             $m->subject("New Subscriber");
