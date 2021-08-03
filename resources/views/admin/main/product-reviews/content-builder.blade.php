c2cdbdced59e8b6b90f83b0a1195d47d313b138b
Mehran Ali <mehran@caresocius.com>
2021-08-03 12:45:12 +0500

    changes in content-builder

--- resources/views/admin/main/product-reviews/content-builder.blade.php
+++ resources/views/admin/main/product-reviews/content-builder.blade.php
@@ -137,7 +137,7 @@
                                 toolbar: "left",
                                 container: '.container',
 			row: 'row',
-			cols: ['col-md-1', 'col-md-2', 'col-md-3', 'col-md-4', 'col-md-5', 'col-md-6', 'col-md-7', 'col-md-8', 'col-md-9', 'col-md-10', 'col-md-11', 'col-md-12'] 
+			cols: ['col-md-1', 'col-md-2', 'col-md-3', 'col-md-4', 'col-md-5', 'col-md-6', 'col-md-7', 'col-md-8', 'col-md-9', 'col-md-10', 'col-md-11', 'col-md-12'] ,
                                 iconselect: "/vendor/content-builder/assets/ionicons/selecticon.html",
                                 snippetPathReplace: ['assets/minimalist-basic/', 'assets/minimalist-basic/'],
                                 snippetCategories: [
