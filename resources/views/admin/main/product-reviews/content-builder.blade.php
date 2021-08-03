959a89d9f3ed0e00e32578597d45b88ac7f64e6f
Mehran Ali <mehran@caresocius.com>
2021-08-03 12:17:11 +0500

    content-builder changes

--- resources/views/admin/main/product-reviews/content-builder.blade.php
+++ resources/views/admin/main/product-reviews/content-builder.blade.php
@@ -143,36 +143,36 @@
                                     [36,"Color Background"],
                                     [1,"Title"],
                                     /*[2,"Title, Subtitle"],*/
-                                    /*[3,"Info, Title"],
-                                    [4,"Info, Title, Subtitle"],*/
+                                    /*[3,"Info, Title"],*/
+                                    [4,"Info, Title, Subtitle"],
                                     [5,"Heading, Paragraph"],
                                     [6,"Paragraph"],
                                     [7,"Paragraph, Images + Caption"],
                                     [8,"Heading, Paragraph, Images + Caption"],
                                     [33,"Buttons"],
-                                    /*[34,"Cards"],*/
+                                    [34,"Cards"],
                                     [9,"Images + Caption"],
                                     [10,"Images + Long Caption"],
                                     [11,"Images"],
                                     [12,"Single Image"],
-                                    /*[13,"Call to Action"],
-                                    [14,"List"],*/
+                                    [13,"Call to Action"],
+                                    [14,"List"],
                                     [15,"Quotes"],
-                                    /*[16,"Profile"],
-                                    [17,"Map"],*/
+                                    [16,"Profile"],
+                                    /*[17,"Map"],*/
                                     [20,"Video"],
-                                    /*[18,"Social"],
+                                    [18,"Social"],
                                     [21,"Services"],
                                     [22,"Contact Info"],
-                                    [23,"Pricing"],*/
+                                    /*[23,"Pricing"],*/
                                     [24,"Team Profile"],
                                     [25,"Products/Portfolio"],
-                                    /*[26,"How It Works"],
+                                    [26,"How It Works"],
                                     [27,"Partners/Clients"],
                                     [28,"As Featured On"],
                                     [29,"Achievements"],
                                     [32,"Skills"],
-                                    [30,"Coming Soon"],
+                                    /*[30,"Coming Soon"],
                                     [31,"Page Not Found"],*/
                                     [19,"Separator"],
 
